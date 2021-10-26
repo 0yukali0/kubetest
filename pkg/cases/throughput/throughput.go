a6ae0815cdfd203f98b9cdf02f93c1f1d2f0e6d5
0yukali0 <a08h0283@gmail.com>
2021-10-26 18:17:21 +0800

    batch

--- pkg/cases/throughput/throughput.go
+++ pkg/cases/throughput/throughput.go
@@ -1,89 +1,118 @@
 package main
 
 import (
-    "fmt"
-    "github.com/TaoYang526/kubetest/pkg/cache"
-    "github.com/TaoYang526/kubetest/pkg/collector"
-    "github.com/TaoYang526/kubetest/pkg/common"
-    "github.com/TaoYang526/kubetest/pkg/kubeclient"
-    "github.com/TaoYang526/kubetest/pkg/monitor"
-    "github.com/TaoYang526/kubetest/pkg/painter"
-    "time"
+	"fmt"
+	"time"
+
+	"github.com/TaoYang526/kubetest/pkg/cache"
+	"github.com/TaoYang526/kubetest/pkg/collector"
+	"github.com/TaoYang526/kubetest/pkg/common"
+	"github.com/TaoYang526/kubetest/pkg/kubeclient"
+	"github.com/TaoYang526/kubetest/pkg/monitor"
+	"github.com/TaoYang526/kubetest/pkg/painter"
 )
 
 const (
-    AppName = "throughput.test"
-    ChartTitle = "Scheduling Throughput"
-    ChartXLabel = "Seconds"
-    ChartYLabel = "Number of Pods"
+	AppName     = "throughput.test"
+	ChartTitle  = "Scheduling Throughput"
+	ChartXLabel = "Seconds"
+	ChartYLabel = "Number of Pods"
 )
 
 var (
-    PodNum = 50000
-    SelectPodLabels = map[string]string{cache.KeyApp: AppName}
+	TotolPodNum     = 50000
+	DeploymentNum   = 10
+	PodNum          = TotolPodNum / DeploymentNum
+	SelectPodLabels = map[string]string{cache.KeyApp: AppName}
+	Monitors        = make([]*monitor.Monitor, DeploymentNum)
+	MonitorID       = 1
 )
 
 func main() {
-    // make sure all related pods are cleaned up
-    monitor.WaitUtilAllMetricsAreCleanedUp(collectDeploymentMetrics)
+	// make sure all related pods are cleaned up
+	monitor.WaitUtilAllMetricsAreCleanedUp(collectDeploymentMetrics)
 
-    dataMap := make(map[string][]int, 2)
-    for _, schedulerName := range common.SchedulerNames {
-        fmt.Printf("Starting %s via scheduler %s\n", AppName, schedulerName)
-        // create deployment
-        deployment := cache.KubeDeployment{}.WithSchedulerName(schedulerName).WithAppName(
-            AppName).WithPodNum(int32(PodNum)).Build()
-        kubeclient.CreateDeployment(common.Namespace, deployment)
-        beginTime := time.Now().Truncate(time.Second)
-        // start monitor
-        createMonitor := &monitor.Monitor{
-            Name:           AppName + " create-monitor",
-            Interval:       1,
-            CollectMetrics: collectDeploymentMetrics,
-            SkipSameMerics: true,
-            StopTrigger: func(m *monitor.Monitor) bool {
-                lastCp := m.GetLastCheckPoint()
-                if lastCp.MetricValues[2] == PodNum {
-                    // stop monitor when readyReplicas equals PodNum
-                    return true
-                }
-                return false
-            },
-        }
-        createMonitor.Start()
-        // wait util this deployment is running successfully
-        createMonitor.WaitForStopped()
-        // calculate distribution of pod start times
-        endTime := time.Now()
-        podStartTimes := collector.CollectPodInfo(common.Namespace,
-            kubeclient.GetListOptions(SelectPodLabels), collector.ParsePodStartTime)
-        podStartTimeDistribution := collector.AnalyzeTimeDistribution(beginTime, endTime, podStartTimes)
-        fmt.Printf("Distribution of pod start times: %v, seconds: %d beginTime: %v, endTime: %v \n",
-            podStartTimeDistribution, len(podStartTimeDistribution), beginTime, endTime)
+	dataMap := make(map[string][]int, 2)
+	for _, schedulerName := range common.SchedulerNames {
+		fmt.Printf("Starting %s via scheduler %s\n", AppName, schedulerName)
+		// create deployment
+		beginTime := time.Now().Truncate(time.Second)
+		for MonitorID = 1; MonitorID <= DeploymentNum; MonitorID++ {
+			target := fmt.Sprintf("%s%d", AppName, MonitorID)
+			deployment := cache.KubeDeployment{}.WithSchedulerName(schedulerName).WithAppName(
+				target).WithPodNum(int32(PodNum)).Build()
+			kubeclient.CreateDeployment(common.Namespace, deployment)
+			// start monitor
+			createMonitor := &monitor.Monitor{
+				Name:           AppName + " create-monitor" + fmt.Sprintf("%d", MonitorID),
+				Interval:       15,
+				CollectMetrics: collectDeploymentMetrics,
+				SkipSameMerics: true,
+				StopTrigger: func(m *monitor.Monitor) bool {
+					lastCp := m.GetLastCheckPoint()
+					if lastCp.MetricValues[2] == PodNum {
+						// stop monitor when readyReplicas equals PodNum
+						return true
+					}
+					return false
+				},
+			}
+			Monitors[MonitorID-1] = createMonitor
+			createMonitor.Start()
+		}
+		// wait all deployments running
+		for MonitorID = 1; MonitorID <= DeploymentNum; MonitorID++ {
+			// wait util this deployment is running successfully
+			Monitors[MonitorID-1].WaitForStopped()
+		}
+		// calculate distribution of pod start times
+		endTime := time.Now()
+		for MonitorID = 1; MonitorID <= DeploymentNum; MonitorID++ {
 
-        // Save checkpoints
-        dataMap[common.SchedulerAlias[schedulerName]] = podStartTimeDistribution
+		}
+		var podStartTimes []interface{}
+		for MonitorID = 1; MonitorID <= DeploymentNum; MonitorID++ {
+			target := fmt.Sprintf("%s%d", AppName, MonitorID)
+			targetMap := map[string]string{cache.KeyApp: target}
+			podStartTime := collector.CollectPodInfo(common.Namespace,
+				kubeclient.GetListOptions(targetMap), collector.ParsePodStartTime)
+			podStartTimes = append(podStartTimes, podStartTime)
+		}
+		/*
+			podStartTimes := collector.CollectPodInfo(common.Namespace,
+				kubeclient.GetListOptions(SelectPodLabels), collector.ParsePodStartTime)
+		*/
+		podStartTimeDistribution := collector.AnalyzeTimeDistribution(beginTime, endTime, podStartTimes)
+		fmt.Printf("Distribution of pod start times: %v, seconds: %d beginTime: %v, endTime: %v \n",
+			podStartTimeDistribution, len(podStartTimeDistribution), beginTime, endTime)
 
-        // delete deployment
-        kubeclient.DeleteDeployment(common.Namespace, AppName)
-        // make sure all related pods are cleaned up
-        monitor.WaitUtilAllMetricsAreCleanedUp(collectDeploymentMetrics)
-    }
+		// Save checkpoints
+		dataMap[common.SchedulerAlias[schedulerName]] = podStartTimeDistribution
 
-    // draw chart
-    linePoints := painter.GetLinePoints(dataMap)
-    chart := &painter.Chart{
-        Title:      ChartTitle,
-        XLabel:     ChartXLabel,
-        YLabel:     ChartYLabel,
-        Width:      common.ChartWidth,
-        Height:     common.ChartHeight,
-        LinePoints: linePoints,
-        SvgFile:    common.ChartSavePath + "throughput.svg",
-    }
-    painter.DrawChart(chart)
+		// delete deployment
+		for MonitorID = 1; MonitorID <= DeploymentNum; MonitorID++ {
+			target := fmt.Sprintf("%s%d", AppName, MonitorID)
+			kubeclient.DeleteDeployment(common.Namespace, target)
+		}
+		// make sure all related pods are cleaned up
+		// monitor.WaitUtilAllMetricsAreCleanedUp(collectDeploymentMetrics)
+	}
+
+	// draw chart
+	linePoints := painter.GetLinePoints(dataMap)
+	chart := &painter.Chart{
+		Title:      ChartTitle,
+		XLabel:     ChartXLabel,
+		YLabel:     ChartYLabel,
+		Width:      common.ChartWidth,
+		Height:     common.ChartHeight,
+		LinePoints: linePoints,
+		SvgFile:    common.ChartSavePath + "throughput.svg",
+	}
+	painter.DrawChart(chart)
 }
 
 func collectDeploymentMetrics() []int {
-    return collector.CollectDeploymentMetrics(common.Namespace, AppName)
+	target := fmt.Sprintf("%s%d", AppName, MonitorID)
+	return collector.CollectDeploymentMetrics(common.Namespace, target)
 }
--- pkg/common/const.go
+++ pkg/common/const.go
@@ -3,25 +3,25 @@ package common
 import "gonum.org/v1/plot/vg"
 
 const (
-    // constants for testing environment
-    QueueName        = "root.sandbox"
-    Namespace        = "default"
-    HollowNodePrefix = "hollow-"
+	// constants for testing environment
+	QueueName        = "root.sandbox"
+	Namespace        = "default"
+	HollowNodePrefix = "hollow-"
 
-    // constants for yunikorn scheduler
-    YSConfigMapNamespace            = "default"
-    YSConfigMapName                 = "yunikorn-configs"
-    YSConfigMapQueuesYamlKey        = "queues.yaml"
-    YSConfigMapQueuesResourceMemKey = "memory"
-    YSName                          = "yunikorn"
+	// constants for yunikorn scheduler
+	YSConfigMapNamespace            = "yunikorn"
+	YSConfigMapName                 = "yunikorn-configs"
+	YSConfigMapQueuesYamlKey        = "queues.yaml"
+	YSConfigMapQueuesResourceMemKey = "memory"
+	YSName                          = "yunikorn"
 
-    // constants for chart
-    ChartWidth    = 6 * vg.Inch
-    ChartHeight   = 6 * vg.Inch
-    ChartSavePath = "/tmp/"
+	// constants for chart
+	ChartWidth    = 6 * vg.Inch
+	ChartHeight   = 6 * vg.Inch
+	ChartSavePath = "/tmp/"
 )
 
 var (
-    SchedulerNames = []string{YSName, ""}
-    SchedulerAlias = map[string]string{YSName: YSName, "": "k8s"}
+	SchedulerNames = []string{YSName, ""}
+	SchedulerAlias = map[string]string{YSName: YSName, "": "k8s"}
 )
